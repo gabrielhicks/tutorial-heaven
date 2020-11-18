876406748a234d8f2d996cad39abef9b97e158dd
gabrielhicks <gabrielhicks@mail.com>
2020-11-18 09:21:26 -0500

    initial commit and push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { connect } from "react-redux"
+import {
+  increaseCounter,
+  decreaseCounter,
+} from "./redux/Counter/counter.actions"
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Count: {props.count}</div>
+      <button onClick={() => props.increaseCounter()}>Increase Count</button>
+      <button onClick={() => props.decreaseCounter()}>Decrease Count</button>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    count: state.counter.count,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    increaseCounter: () => dispatch(increaseCounter()),
+    decreaseCounter: () => dispatch(decreaseCounter()),
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
